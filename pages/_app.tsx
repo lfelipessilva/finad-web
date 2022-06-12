70bd1af9a3ab01a9163fcc3ca82673bb0570c31b
Luis Felipe <lfelipe.silva@aterapia.com.br>
2022-06-11 21:11:41 -0300

    add styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0px;
+    margin: 0px;
+    background: #4c4c4c;
+    color: #f0f0f0;
+    font-family: proxima-nova, sans-serif
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#806060",
+  },
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #1c1c1c;
+`
+
 export default function Home() {
   return ( 
-    <h1>Hello World!</h1>
+    <Title>Hello World!</Title>
   )
 }
