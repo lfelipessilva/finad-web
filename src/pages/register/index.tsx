7af4f8dfa774555f4fa032fcb6251745fe35671b
Luis Felipe <luis.felipeszsv@gmail.com>
2023-01-07 12:10:29 -0300

    send request headers content type as application/json

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -103,6 +103,9 @@ export default function Home() {
 
     await fetch(`https://api.finad.devluis.tech/user`, {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         name: `${name} ${lastName}`,
         email: email,
