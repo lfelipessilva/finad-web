fecaef6e9aca17c9724e859fc853be07ac2a70ea
Luís Felipe <lfelipe.silva@aterapia.com.br>
2023-01-06 22:46:39 -0300

    try using string on request

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -101,7 +101,7 @@ export default function Home() {
   const submitForm = async (e: FormEvent) => {
     e.preventDefault();
 
-    await fetch(`${process.env.NEXT_PUBLIC_API_ENTRYPOINT}/user`, {
+    await fetch(`https://api.finad.devluis.tech/user`, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
