6ae35c552259fa4dd5dbc80a9ac2620f8d7d7153
luis.felipeszsv@gmail.com <luis.felipeszsv@gmail.com>
2023-01-15 18:22:20 -0300

    add expense service and types

---
+++ src/services/expenseService.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { Expense } from "../types/Expense";
+import { apiClient } from ".";
+
+const createExpense = async (expense: Expense) => {
+  const response = await apiClient.post<any>("/expense", expense);
+  return response;
+}
+
+const findAll = async () => {
+  const response = await apiClient.get<Expense[]>("/expense");
+  return response.data;
+}
+
+const findById = async (id: string) => {
+  const response = await apiClient.get<Expense>(`/expense/${id}`);
+  return response;
+}
+
+const update = async (id: string, { value, description, date }: Expense) => {
+  const response = await apiClient.put<any>(`/expense/${id}`, { value, description, date });
+  return response;
+}
+
+const deleteById = async (id: string) => {
+  const response = await apiClient.delete<any>(`/expense/${id}`);
+  return response;
+}
+
+const deleteAll = async () => {
+  const response = await apiClient.delete<any>("/expense");
+  return response;
+}
+
+const ExpenseService = {
+  createExpense,
+  findAll,
+  findById,
+  update,
+  deleteById,
+  deleteAll
+}
+
+export default ExpenseService;
---
+++ src/types/Expense.ts
@@ -0,0 +1,9 @@
+export type Expense = {
+  id: string;
+  userId: string;
+  value: number;
+  date: Date;
+  description: string;
+  created_at: Date;
+  updated_at: Date;
+};
