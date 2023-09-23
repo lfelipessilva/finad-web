b572ccc7be99e3a4bb60b1b592ef53701b87a638
luis.felipeszsv@gmail.com <luis.felipeszsv@gmail.com>
2023-09-22 22:46:14 -0300

    feat: add account to create income modal

--- src/components/modals/CreateIncomeModal.tsx
+++ src/components/modals/CreateIncomeModal.tsx
@@ -1,103 +1,128 @@
-import React, { ChangeEvent, useState } from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
+import React, { ChangeEvent, useState } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 import {
   ArrowCircleUp as ArrowCircleUpIcon,
-  X as CloseIcon
-} from 'phosphor-react'
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-import { toast } from 'react-toastify';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod'
-import { ICreateIncome } from '../../types/Income';
-import IncomeService from '../../services/incomeService';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { MaskMoney } from '../../utils/masks';
-import { FormInput } from '../form/FormInput';
-import { FormToggle } from '../form/FormToggle';
-import { FormSelect } from '../form/FormSelect';
-import { FormDate } from '../form/FormDate';
-import CategoryService from '../../services/categoryService';
-import PrimaryButton from '../buttons/PrimaryButton';
+  X as CloseIcon,
+} from "phosphor-react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { toast } from "react-toastify";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ICreateIncome } from "../../types/Income";
+import IncomeService from "../../services/incomeService";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { MaskMoney } from "../../utils/masks";
+import { FormInput } from "../form/FormInput";
+import { FormToggle } from "../form/FormToggle";
+import { FormSelect } from "../form/FormSelect";
+import { FormDate } from "../form/FormDate";
+import CategoryService from "../../services/categoryService";
+import PrimaryButton from "../buttons/PrimaryButton";
+import AccountService from "../../services/accountService";
 
 const defaultValues = {
-  value: 'R$00,00',
-  date: new Date()
-}
+  value: "R$00,00",
+  date: new Date(),
+};
 
-const validationSchema = z
-  .object({
-    value: z
-      // @ts-ignore
-      .custom<string>(string => string.match(/^R\$.+\d{2}$/) && Number(string.replace(/\D/g, '')) > 0, "O valor deve ser maior que 0!") // this regex tests for R$*,00
-      .transform(value => Number(value.replace(/\D/g, ''))),
-    status: z
-      .boolean({
-        required_error: 'O status deve ser marcado!',
-        invalid_type_error: 'Algo deu errado :/'
-      })
-      .transform(value => value ? 'received' : 'unreceived'),
-    description: z
-      .string({
-        required_error: 'A descrição deve ser preenchida!',
-        invalid_type_error: 'Algo deu errado :/'
-      })
-      .min(1, { message: "Deve informar a descrição!" }),
-    date: z.date({
-      required_error: 'A data deve ser preenchida!',
-      invalid_type_error: 'Algo deu errado :/'
-    }),
-    categoryId: z.string({
-      required_error: 'A categoria deve ser preenchida!',
-      invalid_type_error: 'Algo deu errado :/'
+const validationSchema = z.object({
+  value: z
+    // @ts-ignore
+    .custom<string>(
+      (string) =>
+        string.match(/^R\$.+\d{2}$/) && Number(string.replace(/\D/g, "")) > 0,
+      "O valor deve ser maior que 0!"
+    ) // this regex tests for R$*,00
+    .transform((value) => Number(value.replace(/\D/g, ""))),
+  status: z
+    .boolean({
+      required_error: "O status deve ser marcado!",
+      invalid_type_error: "Algo deu errado :/",
     })
-  })
+    .transform((value) => (value ? "received" : "unreceived")),
+  description: z
+    .string({
+      required_error: "A descrição deve ser preenchida!",
+      invalid_type_error: "Algo deu errado :/",
+    })
+    .min(1, { message: "Deve informar a descrição!" }),
+  date: z.date({
+    required_error: "A data deve ser preenchida!",
+    invalid_type_error: "Algo deu errado :/",
+  }),
+  categoryId: z.string({
+    required_error: "A categoria deve ser preenchida!",
+    invalid_type_error: "Algo deu errado :/",
+  }),
+  accountId: z.string({
+    required_error: "A conta deve ser preenchida!",
+    invalid_type_error: "Algo deu errado :/",
+  }),
+});
 
-type FormValues = z.input<typeof validationSchema>
-type SubmitValues = z.output<typeof validationSchema>
+type FormValues = z.input<typeof validationSchema>;
+type SubmitValues = z.output<typeof validationSchema>;
 
 export const CreateIncomeModal = () => {
-  const queryClient = useQueryClient()
-  const [open, setOpen] = useState(false)
-  const { register, handleSubmit, setValue, formState: { errors }, reset: resetForm } = useForm<FormValues>({
+  const queryClient = useQueryClient();
+  const [open, setOpen] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+    reset: resetForm,
+  } = useForm<FormValues>({
     defaultValues: defaultValues,
-    resolver: zodResolver(validationSchema)
+    resolver: zodResolver(validationSchema),
   });
 
   const { data: categories } = useQuery({
-    queryKey: ['category'],
+    queryKey: ["category"],
     queryFn: async () => {
-      return CategoryService.findAll()
-    }
-  })
+      return CategoryService.findAll();
+    },
+  });
+
+  const { data: accounts } = useQuery({
+    queryKey: ["account"],
+    queryFn: async () => {
+      return AccountService.findAll();
+    },
+  });
 
   const createIncome = useMutation(
     async (income: ICreateIncome) => await IncomeService.createIncome(income),
     {
       onSuccess: (data) => {
-        toast.success('Receita criada com sucesso', {
-          position: 'top-center',
-        })
+        toast.success("Receita criada com sucesso", {
+          position: "top-center",
+        });
 
-        queryClient.invalidateQueries({ queryKey: 'transactions' })
-        queryClient.invalidateQueries({ queryKey: 'transactions_balances' })
+        queryClient.invalidateQueries({ queryKey: "transactions" });
+        queryClient.invalidateQueries({ queryKey: "transactions_balances" });
 
-        resetForm()
+        resetForm();
 
-        setOpen(false)
+        setOpen(false);
       },
       onError: (error: any) => {
-        toast.error('Houve um problema ao criar receita :/', {
-          position: 'top-center',
-        })
+        toast.error("Houve um problema ao criar receita :/", {
+          position: "top-center",
+        });
       },
     }
   );
 
-  const onSubmit: SubmitHandler<SubmitValues> = (data) => createIncome.mutate(data)
+  const onSubmit: SubmitHandler<SubmitValues> = (data) =>
+    createIncome.mutate(data);
 
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
-      <Dialog.Trigger asChild className="hover:cursor-pointer hover:opacity-30 opacity-100 transition-all duration-100">
+      <Dialog.Trigger
+        asChild
+        className="transition-all duration-100 opacity-100 hover:cursor-pointer hover:opacity-30"
+      >
         <ArrowCircleUpIcon size={32} />
       </Dialog.Trigger>
       <Dialog.Portal>
@@ -114,15 +139,18 @@ export const CreateIncomeModal = () => {
               <CloseIcon />
             </div>
           </Dialog.Close>
-          {/* @ts-ignore */}
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-100">
+          <form
+            // @ts-ignore
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 w-100"
+          >
             <FormInput
               register={register}
               name="value"
               type="text"
               placeholder="Valor"
               onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                setValue('value', MaskMoney(event.target.value))
+                setValue("value", MaskMoney(event.target.value));
               }}
               error={errors.value}
             />
@@ -146,13 +174,20 @@ export const CreateIncomeModal = () => {
               error={errors.description}
             />
             <FormSelect
+              name="accountId"
+              register={register}
+              options={accounts}
+              error={errors.accountId}
+            />
+
+            <FormSelect
               name="categoryId"
               register={register}
               options={categories}
               error={errors.categoryId}
             />
             <Dialog.Close asChild>
-              <button className="flex flex-row justify-center items-center selection:items-center rounded-xl bg-blue-500 p-3 text-2xl text-white bg-gray-700 font-semibold hover:opacity-80 transition-all duration-200">
+              <button className="flex flex-row items-center justify-center p-3 text-2xl font-semibold text-white transition-all duration-200 bg-gray-700 bg-blue-500 selection:items-center rounded-xl hover:opacity-80">
                 Cancelar
               </button>
             </Dialog.Close>
@@ -162,6 +197,6 @@ export const CreateIncomeModal = () => {
           </form>
         </Dialog.Content>
       </Dialog.Portal>
-    </Dialog.Root >
-  )
+    </Dialog.Root>
+  );
 };
